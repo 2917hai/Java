41bfb35eae9ea676c06b470f63eee25460284d54
Umesh <yadavumeshkumar815@gmail.com>
2025-02-07 19:34:34 +0530

    pattern

---
+++ Question1.java
@@ -0,0 +1,25 @@
+public class Question1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("///////////////////////////////////////////////////////////////////////////////////////////");
+        for(int ii=1; ii<=5; ii++)
+        {
+            for(int jj=1; jj<=5; jj++)
+            {
+                if(jj<=ii)
+                {
+                    System.out.print("*");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
