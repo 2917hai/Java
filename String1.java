28ef18e9ed43a26854da7eaecf82f099c01e0750
Umesh <yadavumeshkumar815@gmail.com>
2025-02-28 19:45:57 +0530

    String

--- String1.java
+++ String1.java
@@ -1,9 +1,10 @@
 /*
  * String is non-primitive data type 
  * String is a class 
+ * String are immutable that means we can not change the value of String 
  * 
- * String name="umesh " Or
- * String name=new String("Umesh")
+ * String name="umesh " Or        data are store in string pool
+ * String name=new String("Umesh")     data store in heap memory while we using new keyword  by default heap me hota hai
  * char name[]={'U','M','E','S','H'}
  */
 public class String1 {
@@ -18,11 +19,13 @@ public class String1 {
         System.out.println(name);       //  First
         System.out.println(name1);      // Second
         System.out.println(name2);      // Third
+
         for(char name3:name2)
         {
           System.out.print(name3);
         }
         System.out.println();
+
         System.out.println(name4);
 
 
