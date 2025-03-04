214eefe1dd1bd3e41abe32c37dd8a0481d02118b
Umesh <yadavumeshkumar815@gmail.com>
2025-03-04 12:25:35 +0530

    reverse String

---
+++ String2.java
@@ -0,0 +1,51 @@
+public class String2 {
+    public static void main(String[] args) { 
+        System.out.println("String class");
+        String obj1="Apple";          // data are store in string pool and create single object
+        String obj2=new String("Orange");   //data are store in heap and create 2 object
+        String obj3="Apple";               // no object create because data are store in string pool
+        String obj4="Orange";        //no object create because data are also store in spring pool
+        if(obj1==obj3)
+        {
+            System.out.println("true");
+
+        }
+         else
+         {
+           System.out.println("false");
+         }
+
+         if(obj2==obj4)   
+         {
+             System.out.println("true");
+ 
+         }
+          else
+          {
+            System.out.println("false");
+          }
+          
+          obj1.replace("Apple", "Orange");// data are store in heap because we use method 
+          System.out.println(obj1);//  Apple
+          obj1=obj1.replace("Apple", "Orange");// data are store in heap because we use method
+          System.out.println(obj1);// Orange
+
+      
+        char[] ch=obj1.toCharArray();
+        int length=obj1.length();
+        int a=0;
+        int b=length-1;
+        System.out.println(a+""+b);
+        int mid=length/2;
+        for(int i=0;i<mid;i++)
+        {
+            char temp=ch[a];
+            ch[a]=ch[b];
+            ch[b]=temp;
+            a++;
+            b--;
+        }
+        System.out.println(ch);
+        
+    }
+}
