287108b1a3e23682bc6b0035dd7c6e32673bc8e4
Umesh <yadavumeshkumar815@gmail.com>
2025-04-28 18:48:59 +0530

    duplicate data in a array

---
+++ Indrapuri/Dupicate.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Dupicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s;
+        System.out.println("enter the size of arr");
+        s=sc.nextInt();
+        int arr[]=new int[s];
+        System.out.println("enter the data of arrr");
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("HEre your arr data");
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.println("Duplicate found: "+arr[i]);
+                    arr[i]=0;
+                }
+            }
+        }
+        for(int i=0; i<arr.length; i++)
+        {
+           
+            System.out.println(arr[i]);
+        }
+      
+        
+    }
+}
